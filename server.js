c663e1861253b1f2839a2eaeee9df6796d23f035
Alexander <alexander.krist@gmail.com>
2025-07-28 00:13:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/admin/news', require('./routes/adminNewsRoutes'));
 app.use('/admin/properties', require('./routes/propertyRoutes'));
 app.use('/property', require('./routes/propertyRoutes'));
 app.use('/admin/renovations', require('./routes/renovationRoutes'));
-//app.use('/', require('./routes/homeRoutes')); // loads the new home page with news, properties, renovations
+app.use('/', require('./routes/homeRoutes')); // loads the new home page with news, properties, renovations
 // Server
 // const PORT = process.env.PORT || 3000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
